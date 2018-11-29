f009710e440cb2b19f3b3a1dc94273e242dab7b1
Alaeddine Chemoune <achemoune@emi.u-bordeaux1.fr>
2018-11-29 16:02:46 +0100

    tp9-10/

--- tp9-10/spark_maven/src/main/java/TPSpark.java
+++ tp9-10/spark_maven/src/main/java/TPSpark.java
@@ -1,6 +1,11 @@
+import java.util.Arrays;
+
 import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+
+import scala.Tuple2;
 
 public class TPSpark {
 
@@ -9,8 +14,15 @@ public class TPSpark {
 		SparkConf conf = new SparkConf().setAppName("TPSpark");
 		JavaSparkContext context = new JavaSparkContext(conf);
 		
+		//JavaRDD<String> textFile = context.textFile("hdfs://localhost:9000/user/raw_data/worldcitiespop.txt");
+		//System.out.println("le nombre de partitions : "+textFile.getNumPartitions());
+		
+		
 		JavaRDD<String> textFile = context.textFile("hdfs://localhost:9000/user/raw_data/worldcitiespop.txt");
-		System.out.println("le nombre de partitions : "+textFile.getNumPartitions());
+		JavaPairRDD<String, Integer> counts = textFile.flatMap(s -> Arrays.asList(s.split(" ")).iterator())
+		    .mapToPair(word -> new Tuple2<>(word, 1))
+		    .reduceByKey((a, b) -> a + b);
+		counts.saveAsTextFile("hdfs://localhost:9000/user/achemoune/test1");
 		
 	}
 	
